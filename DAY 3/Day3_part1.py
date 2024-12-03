e55e6dbb9b2162da4c6ee100d85a8f987a254522
Mel <meledunetworking@gmail.com>
2024-12-03 20:17:34 +0200

    Add files via upload

---
+++ DAY 3/Day3_part1.py
@@ -0,0 +1,13 @@
+import re
+
+# Read text file levels.txt from DAY 3 folder
+file = open("DAY 3/mul.txt", "r").read()
+
+mul_numbers = re.findall(r"mul\((\d+),(\d+)\)", file)
+
+total_sum = 0
+
+for item in mul_numbers:
+    total_sum += int(item[0]) * int(item[1])
+
+print(total_sum)
