3fee371208ed91a9e62b904f2873aae22a954d78
Mel <meledunetworking@gmail.com>
2024-12-08 19:13:14 +0200

    Add files via upload

---
+++ DAY 5/Day5_part2.py
@@ -0,0 +1,23 @@
+import functools
+
+file = open("DAY 5/page_numbers.txt", "r").read()
+part1, part2 = file.split("\n\n")
+rules = [tuple(map(int, item.split("|"))) for item in part1.split()]
+data = [list(map(int, item.split(","))) for item in part2.split()]
+result = 0
+
+def compare_rules(a, b):
+    if (a, b) in rules:
+        return -1
+    elif (b, a) in rules:
+        return 1
+    
+    return 0
+
+for original_item in data:
+    new_item = sorted(original_item, key=functools.cmp_to_key(compare_rules))
+
+    if original_item != new_item:
+        result = result + new_item[len(new_item) // 2]
+
+print(result)
